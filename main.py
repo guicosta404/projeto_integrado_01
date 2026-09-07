9ab3a980fef0fb8557034585a58aff2345c80f32
Luiz Guilherme <lu.costa1402@gmail.com>
2026-09-07 13:03:17 -0300

    Adicionada classe Usuario.

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+from excecoes import LivroIndisponivelError
+
+
 class Livro:
-    def __init__(self, titulo, autor, ano, copias_totais, copias_disponiveis):
+    def __init__(self, titulo: str, autor: str, ano: int, copias_totais: int):
         self.titulo = titulo
         self.autor = autor
         self.ano = ano
         self.copias_totais = copias_totais
-        self.copias_disponiveis = copias_disponiveis
+        self.copias_disponiveis = copias_totais
 
     def estou_disponivel(self):
         if self.copias_disponiveis <= 0:
@@ -17,16 +20,16 @@ class Livro:
             raise LivroIndisponivelError(f"{self.titulo} não está disponível.")
         self.copias_disponiveis -= 1
 
-
-
     def __str__(self):
         return f"- Título: {self.titulo}\n- Autor: {self.autor}\n- Ano de lançamento: {self.ano}"
 
-class LivroIndisponivelError(Exception):
-    pass
 
-l1 = Livro("HP", "JK", 2000, 2, 2)
-l1.diminuir_copia()
-l1.diminuir_copia()
-l1.diminuir_copia()
+class Usuario:
+    def __init__(self, nome: str, id_usuario: int, telefone: str):
+        self.nome = nome
+        self.id_usuario = id_usuario
+        self.telefone = telefone
+
+    def __str__(self):
+        return f"Nome: {self.nome} - ID: {self.id_usuario} - Tel.: {self.telefone}"
 

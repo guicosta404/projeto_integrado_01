bf67bcebebf5a748fd0a863c0a6ab10948d4d048
Luiz Guilherme <lu.costa1402@gmail.com>
2026-09-13 17:04:22 -0300

    Adicionado opção de cadastro de livro em console.py

--- console.py
+++ console.py
@@ -1,17 +1,18 @@
-from modelos import Biblioteca, Livro, Usuario
+from modelos import Biblioteca
+from excecoes import LivroJaCadastradoError, LivroIndisponivelError, LivroNaoEncontradoError
 
 lib = Biblioteca()
 
 def main():
     while True:
         print("BIBLIOTECA VIRTUAL")
-        print("\n1 - Cadastrar livro" \
-        "\n2 - Cadastrar usuário" \
-        "\n3 - Emprestar livro" \
-        "\n4 - Devolver livro" \
-        "\n5 - Consultar livros" \
-        "\n6 - Relatórios" \
-        "\n0 - Sair")
+        print("""1 - Cadastrar livro
+        2 - Cadastrar usuário
+        3 - Emprestar livro 
+        4 - Devolver livro
+        5 - Consultar livros 
+        6 - Relatório 
+        0 - Sair""")
 
         entrada = input("Digite um número correspondente: ")
         try:
@@ -25,5 +26,23 @@ def main():
 
         if opt == 0:
             break
+
+        if opt == 1:
+            titulo = str(input("Digite o nome do livro: ").strip())
+            autor = str(input("Digite o autor: ").strip())
+            try:
+                ano = int(input("Digite o ano de lançamento: "))
+
+                copias = int(input("Digite o número de cópias para cadastro: "))
+            except ValueError:
+                print("Digite somente números inteiros!")
+                continue
+            try:
+                lib.cadastrar_livro(titulo=titulo, autor=autor, ano=ano, copias=copias)
+                print("Livro cadastrado com sucesso!")
+            except LivroJaCadastradoError as e:
+                print(e)
+                     
+
 if __name__ == "__main__":
     main()

b90805dc47a2efd33d5e024a3a2eaf1c786f4632
Luiz Guilherme <lu.costa1402@gmail.com>
2026-09-13 17:19:58 -0300

    Adicionado opção de cadastro de usuário em console.py

--- console.py
+++ console.py
@@ -1,5 +1,8 @@
 from modelos import Biblioteca
-from excecoes import LivroJaCadastradoError, LivroIndisponivelError, LivroNaoEncontradoError
+from excecoes import (LivroJaCadastradoError, LivroIndisponivelError, LivroNaoEncontradoError,
+                      UsuarioJaCadastradoError, UsuarioNaoEncontrado, EmprestimoNaoEncontradoError,
+                      DevolucaoError)
+
 
 lib = Biblioteca()
 
@@ -7,12 +10,12 @@ def main():
     while True:
         print("BIBLIOTECA VIRTUAL")
         print("""1 - Cadastrar livro
-        2 - Cadastrar usuário
-        3 - Emprestar livro 
-        4 - Devolver livro
-        5 - Consultar livros 
-        6 - Relatório 
-        0 - Sair""")
+2 - Cadastrar usuário
+3 - Emprestar livro 
+4 - Devolver livro
+5 - Consultar livros 
+6 - Relatório 
+0 - Sair""")
 
         entrada = input("Digite um número correspondente: ")
         try:
@@ -22,11 +25,13 @@ def main():
             continue
         if opt < 0 or opt > 6:
             print("Digite uma opção válida.")
-            continue       
+            continue
 
+# Sair do programa
         if opt == 0:
             break
 
+# Cadastrar livro
         if opt == 1:
             titulo = str(input("Digite o nome do livro: ").strip())
             autor = str(input("Digite o autor: ").strip())
@@ -42,7 +47,22 @@ def main():
                 print("Livro cadastrado com sucesso!")
             except LivroJaCadastradoError as e:
                 print(e)
-                     
+
+# Cadastrar usuário
+        if opt == 2:
+            nome = str(input("Digite o nome de usuário: ").strip())
+            try:
+                id_usuario = int(input("Digite o id único de usuário: "))
+            except ValueError:
+                print("Digite somente números inteiros.")
+                continue
+            telefone = str(input("Digite o número de telefone: ").strip())
+            try:
+                lib.cadastrar_usuario(nome=nome, id_usuario=id_usuario, telefone=telefone)
+                print("Usuário cadastrado com sucesso!")
+            except UsuarioJaCadastradoError as e:
+                print(e)
+                    
 
 if __name__ == "__main__":
     main()
